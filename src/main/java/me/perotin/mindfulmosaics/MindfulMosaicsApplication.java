49c3fe0644c393b8d9fef834b43c35df06850b28
perotins <maxfuligni@gmail.com>
2024-03-17 20:01:50 -0400

    init

---
+++ src/main/java/me/perotin/mindfulmosaics/MindfulMosaicsApplication.java
@@ -0,0 +1,13 @@
+package me.perotin.mindfulmosaics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindfulMosaicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindfulMosaicsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/perotin/mindfulmosaics/MindfulMosaicsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.perotin.mindfulmosaics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindfulMosaicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
